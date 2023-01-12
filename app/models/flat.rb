afc53101a28b4eb28ff62898c9c7ea72dc9e61bf
Maria Cosmen <mcosme00@gmail.com>
2023-01-12 20:47:17 +0900

    NEW and CREATE actions

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,7 @@
 class Flat < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
+  validates :description, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :price_per_night, presence: true
+  validates :number_of_guests, presence: true
 end
