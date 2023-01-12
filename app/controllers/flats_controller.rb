52201fa68fbbbe23ba3f4b9ec7bd08e38b2dfafe
Maria Cosmen <mcosme00@gmail.com>
2023-01-12 21:10:53 +0900

    SHOW, EDIT, UPDATE and DESTROY actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-
+  before_action :set_flat, only: %i[show edit update destroy]
   def index
     @flats = Flat.all
   end
@@ -11,15 +11,36 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     if @flat.save
-      redirect_to flats_path
+      redirect_to flat_path(@flat)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
